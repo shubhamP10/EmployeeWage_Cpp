770ba930d24404e55547162256913a42925964e4
“Shubham” <shubhampattar2@gmail.com>
2020-08-19 19:49:40 +0530

    ADDED Code To Caculate Employee Daily Wage.

--- employeeWageComputation.cpp
+++ employeeWageComputation.cpp
@@ -5,10 +5,16 @@
 using namespace std;
 
 int main(){
+    int WAGE_PER_HOUR = 20;
+    int HOURS_PER_DAY = 8;
+    int wage = 0;
     srand(time(0));
     int isPresent = (rand() % 2);
-    if(isPresent == 1)
+    if(isPresent == 1){
         cout << "Employee is Present" << endl;
+        wage = WAGE_PER_HOUR * HOURS_PER_DAY;
+        cout << "Wage = " << wage;
+    }
     else
         cout << "Employee is Absent" << endl;
     return 0;
