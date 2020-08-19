da2a5c6468e998caabd74e2c24bcc817249e523f
“Shubham” <shubhampattar2@gmail.com>
2020-08-19 20:12:37 +0530

    ADDED Code To Compute Wage For Part Time And Full Time Employee.

--- employeeWageComputation.cpp
+++ employeeWageComputation.cpp
@@ -7,12 +7,27 @@ using namespace std;
 int main(){
     int WAGE_PER_HOUR = 20;
     int HOURS_PER_DAY = 8;
+    int PART_TIME_HOURS = 4;
     int wage = 0;
     srand(time(0));
     int isPresent = (rand() % 2);
+
     if(isPresent == 1){
         cout << "Employee is Present" << endl;
-        wage = WAGE_PER_HOUR * HOURS_PER_DAY;
+        int jobType = (rand() % 2) + 1;
+        switch (jobType)
+        {
+        case 1:
+            cout << "Part Time" << endl;
+            wage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            break;
+        case 2:
+            cout << "Full Time" << endl;
+            wage = WAGE_PER_HOUR * HOURS_PER_DAY;
+            break;
+        default:
+            break;
+        }
         cout << "Wage = " << wage;
     }
     else
